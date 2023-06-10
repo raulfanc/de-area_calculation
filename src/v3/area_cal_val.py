1d2fcc45c93d43058d76a27c085120a58d356bff
RexHuang <raulfanc@gmail.com>
2023-06-10 17:18:49 +1200

    JSON validation for `keys`

---
+++ src/v3/area_cal_val.py
@@ -0,0 +1,59 @@
+import json
+from .shape_management import ShapeFactory
+
+
+def validate_shape_json(shape_info):
+    """Validate the necessary keys for each shape type."""
+    """added in v3"""
+
+    shape_type = shape_info.get('type')
+
+    # Define necessary keys for each shape
+    shape_keys = {
+        'rectangle': ['width', 'height'],
+        'triangle': ['base', 'height'],
+        'circle': ['radius'],
+    }
+
+    # Check if the shape type is supported
+    if shape_type not in shape_keys:
+        print(f'Unsupported shape type: "{shape_type}"')
+        return False
+
+    # Check if all necessary keys are present for the shape type
+    for key in shape_keys[shape_type]:
+        if key not in shape_info:
+            print(f'Missing key "{key}" for shape type "{shape_type}"')
+            return False
+
+    # If it passes all checks, return True
+    return True
+
+
+def calculate_total_area(json_shapes):
+    """Calculate the total area of all shapes in the given JSON data."""
+
+    total_area = 0
+    for shape_info in json_shapes:
+        shape_dict = json.loads(shape_info)
+
+        # Only calculate area for valid shapes, added in v3
+        if validate_shape_json(shape_dict):
+            shape_object = ShapeFactory.create_shape(shape_dict)
+            total_area += shape_object.area()
+
+    return total_area
+
+
+if __name__ == '__main__':
+    json_shapes_data = [
+        '{"type": "rectangle", "width": 5, "height": 10}',
+        '{"type": "triangle", "base": 2, "height": 3}',
+        '{"type": "circle", "radius": 4}',
+        '{"type": "rectangle", "width": 5, "height": 5}',
+        '{"type": "very round", "width": 5, "height": 5}',  # unsupported shape, not calculated in total area
+        '{"type": "rectangle", "base": 5, "height": 5}',    # upsupported key, not calculated in total area
+    ]
+
+    # print the total area of all shapes with the format of 2 decimal places
+    print(f"{calculate_total_area(json_shapes_data):.2f}")
---
+++ tests/v3/test_shape.py
@@ -0,0 +1,32 @@
+import unittest
+from src.v2.shape_management import ShapeFactory, Rectangle, Circle, Triangle
+
+
+class TestShapes(unittest.TestCase):
+
+    def test_create_rectangle(self):
+        shape_info = {'type': 'rectangle', 'width': 5, 'height': 10}
+        rectangle = ShapeFactory.create_shape(shape_info)
+        self.assertIsInstance(rectangle, Rectangle)
+        self.assertEqual(rectangle.area(), 50)
+
+    def test_create_circle(self):
+        shape_info = {'type': 'circle', 'radius': 4}
+        circle = ShapeFactory.create_shape(shape_info)
+        self.assertIsInstance(circle, Circle)
+        self.assertAlmostEqual(circle.area(), 50.26548245743669)
+
+    def test_create_triangle(self):
+        shape_info = {'type': 'triangle', 'base': 2, 'height': 3}
+        triangle = ShapeFactory.create_shape(shape_info)
+        self.assertIsInstance(triangle, Triangle)
+        self.assertEqual(triangle.area(), 3)
+
+    # def test_create_unknown_shape(self):
+    #     shape_info = {'type': 'unknown', 'side': 5}
+    #     unknown_shape = ShapeFactory.create_shape(shape_info)
+    #     self.assertIsNone(unknown_shape)
+
+
+if __name__ == '__main__':
+    unittest.main()
